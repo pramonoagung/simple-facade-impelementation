4e22a0c418e7e1b9417d2e063ee95f7faf9e0356
Agung Pramono <agung.prasatu@gmail.com>
2018-05-11 18:09:07 +0700

    Add account login

---
+++ Account.java
@@ -0,0 +1,14 @@
+public class Account {
+    private String username = "user", password = "user", realName = "Agung";
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+}
